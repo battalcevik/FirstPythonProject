10f4e4d5136b6a1aca8eaf56f14e0a68d3800e9c
Battal Cevik <battalcevik@gmail.com>
2022-02-01 21:01:57 -0500

    Update is completed for Trivia game

--- trivia_game.py
+++ trivia_game.py
@@ -1,6 +1,5 @@
 # ----------------------
 def trivia_game():
-
     guesses = []
     correct_guesses = 0
     question_num = 1
@@ -8,7 +7,7 @@ def trivia_game():
     for key in questions:
         print("=========")
         print(key)
-        for i in options[question_num-1]:
+        for i in options[question_num - 1]:
             print(i)
         guess = input("Enter (A, B, C or D): ")
         guess = guess.upper()
@@ -16,18 +15,17 @@ def trivia_game():
         correct_guesses += check_answer(questions.get(key), guess)
         question_num += 1
 
-
     display_score(correct_guesses, guesses)
+
 
 # ----------------------
 def check_answer(answer, guess):
-    if answer == guess
+    if answer == guess:
         print("CORRECT")
         return 1
     else:
         print("WRONG!")
         return 0
-
 
 
 # ----------------------
@@ -37,24 +35,46 @@ def display_score(correct_guesses, guesses):
     print("Results: ")
     print("=======")
     print("Answer: ", end="")
+    for i in questions:
+        print(questions.get(i), end="")
+    print()
+
+    print("Guesses: ", end="")
+    for i in guesses:
+        print(i, end="")
+    print()
+
+    score = int((correct_guesses / len(questions)) * 100)
+    print("Your score is: " + str(score) + "%")
 
 
 # ----------------------
 
 def play_again():
-    pass
+    response = input("Do you want to play again? (yes or no):")
+    response = response.upper()
+
+    if response == "YES":
+        return True
+    else:
+        return False
 
 
 questions = {
-    "Who found the Turkey?: ": "Ataturk",
-    "When the Turkey found?: ": 1923,
-    "Who is the most famous celebrity: ": "Cem Yilmaz",
+    "Who found the Turkey?: ": "A",
+    "When the Turkey found?: ": "B",
+    "Who is the most famous celebrity: ": "C",
 }
 
 options = [["A. Ataturk", "B. Fatih Sultan Mehmet", "C. Alparslan", "D. Abdulhamit"],
            ["A. 1071", "B. 1923", "C. 1881", "D. 1829"],
-            ["A. Haluk Bilginer", "B. Yilmaz Guney", "C. Cem Yilmaz", "D. Sertap Erener"],
+           ["A. Haluk Bilginer", "B. Yilmaz Guney", "C. Cem Yilmaz", "D. Sertap Erener"],
            ]
 
-
 trivia_game()
+
+
+while play_again():
+    trivia_game()
+
+print("Bye")
